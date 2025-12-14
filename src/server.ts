66875a039d0c6e4ca028f31acbb24dbb95bfd25d
GuustavoPetry <ogustavopetry@gmail.com>
2025-12-14 11:22:48 -0300

    Operações de INSERT e SELECT

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,26 @@
 import fastify from "fastify";
+import crypto from "node:crypto";
 import { knex } from "./database";
 
 const app = fastify();
 
-app.get("/hello", async () => {
-  const tables = await knex("sqlite_schema").select("*");
+app.get("/add", async () => {
+  const transaction = await knex("transactions").insert({
+    id: crypto.randomUUID(),
+    title: "Transação de Teste 2",
+    amount: 2000
+  })
+  .returning("*");
 
-  return tables;
+  return transaction;
+});
+
+app.get("/search", async () => {
+  const transactions = await knex("transactions")
+    .where("amount", 2000)
+    .select("*");
+
+  return transactions;
 });
 
 app
