5a1d2256a87fcba7efe52424302bf0e317a83089
GuustavoPetry <ogustavopetry@gmail.com>
2025-12-28 13:52:06 -0300

    Registrando @fastify/cookie e inserindo session_id na rota post

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -10,6 +10,24 @@ export async function transactionRoutes(app: FastifyInstance) {
     return { transactions };
   });
 
+  app.get("/:id", async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = getTransactionParamsSchema.parse(request.params);
+
+    const transaction = (await knex("transactions").where("id", id).first());
+
+    return { transaction };
+  });
+
+  app.get("/summary", async () => {
+    const summary = await knex("transactions").sum("amount", { as: "amount" }).first();
+
+    return { summary };
+  });
+
   app.post("/", async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
@@ -19,12 +37,24 @@ export async function transactionRoutes(app: FastifyInstance) {
 
     const { title, amount, type } = createTransactionBodySchema.parse(request.body);
 
+    let sessionId = request.cookies.sessionId;
+
+    if (!sessionId) {
+      sessionId = randomUUID();
+
+      reply.cookie("sessionId", sessionId, {
+        path: "/", // qualquer rota pode acessar o cookie;
+        maxAge: 60 * 60 * 24 * 7, // validade de 7 dias em segundos
+      });
+    }
+
     await knex("transactions").insert({
       id: randomUUID(),
       title,
-      amount: type === "credit" ? amount : amount * -1
+      amount: type === "credit" ? amount : amount * -1,
+      session_id: sessionId
     });
 
-    return reply.status(201).send();
+    return reply.status(201).send(sessionId);
   });
 }
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import fastify from "fastify";
-import crypto from "node:crypto";
-import { knex } from "./database";
+import cookie from "@fastify/cookie";
 import { env } from "./env";
 import { transactionRoutes } from "./routes/transactions";
 
 const app = fastify();
+
+app.register(cookie);
 
 app.register(transactionRoutes, {
   prefix: "transactions"
