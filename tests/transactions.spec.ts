76c799243571141918f99953dbedf30230369660
GuustavoPetry <ogustavopetry@gmail.com>
2025-12-28 18:55:59 -0300

    Testes end-to-end

--- tests/transactions.spec.ts
+++ tests/transactions.spec.ts
@@ -1,8 +1,11 @@
 import { it, beforeAll, afterAll, beforeEach, describe, expect } from "vitest";
-import supertest from "supertest";
+import { execSync } from "node:child_process"; // Executa comandos no terminal via código
+import supertest, { SuperAgentTest } from "supertest";
 import { app } from "../src/app";
 
 describe("Transactions Routes", () => {
+    let agent: supertest.Agent;
+
     beforeAll(async () => {
         await app.ready();
     });
@@ -11,8 +14,18 @@ describe("Transactions Routes", () => {
         await app.close();
     });
 
+    beforeEach(() => {
+        // Agent novo para cada teste -> garante independência
+        agent = supertest.agent(app.server);
+
+        // resetar banco no beforeEach garante independência
+        // Acaba tomando mais tempo -> tests End-to-End
+        execSync("npm run knex migrate:rollback --all");
+        execSync("npm run knex migrate:latest");
+    });
+
     it("should be able to create a new transaction", async () => {
-        await supertest(app.server)
+        await agent
             .post("/transactions")
             .send({
                 title: "New Transaction",
@@ -23,8 +36,6 @@ describe("Transactions Routes", () => {
     });
 
     it("should be able to list all transactions", async () => {
-        const agent = supertest.agent(app.server);
-
         await agent
             .post("/transactions")
             .send({
@@ -46,5 +57,60 @@ describe("Transactions Routes", () => {
         ]);
     });
 
+    it("should be able to get a specific transaction", async () => {
+        await agent
+            .post("/transactions")
+            .send({
+                title: "New Transaction",
+                amount: 5000,
+                type: "credit"
+            });
+
+        const listTransactionResponse =
+            await agent
+                .get("/transactions")
+                .expect(200);
+
+        const transactionId = listTransactionResponse.body.transactions[0].id;
+
+        const getTransactionResponse =
+            await agent
+                .get(`/transactions/${transactionId}`)
+                .expect(200);
+
+        expect(getTransactionResponse.body.transaction).toEqual(
+            expect.objectContaining({
+                title: "New Transaction",
+                amount: 5000
+            })
+        );
+    });
+
+    it("should be able to get the summary", async () => {
+        await agent
+            .post("/transactions")
+            .send({
+                title: "New Transaction",
+                amount: 5000,
+                type: "credit"
+            });
+
+        await agent
+            .post("/transactions")
+            .send({
+                title: "New Transaction",
+                amount: 3000,
+                type: "debit"
+            });
+
+        const summaryResponse =
+            await agent
+                .get("/transactions/summary")
+                .expect(200);
+
+        expect(summaryResponse.body.summary).toEqual({
+            amount: 2000
+        });
+    });
 });
 
