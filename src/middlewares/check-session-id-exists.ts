e98bcacce0788d72e551da23c8ecff53069c1e3c
GuustavoPetry <ogustavopetry@gmail.com>
2025-12-28 14:37:10 -0300

    Middleware que verifica se existe session ID nos cookie da requisição

---
+++ src/middlewares/check-session-id-exists.ts
@@ -0,0 +1,20 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+
+declare module "fastify" {
+    interface FastifyRequest {
+        sessionId: string
+    }
+}
+
+export async function checkSessionIdExists(request: FastifyRequest, reply: FastifyReply
+) {
+    const sessionId = request.cookies.sessionId;
+    
+        if (!sessionId) {
+          return reply.status(401).send({
+            error: "Unauthorized"
+          });
+        }
+    
+    request.sessionId = sessionId;
+}
