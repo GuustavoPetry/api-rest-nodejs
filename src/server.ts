4760072044e77fae79633874c8a3ac271f144aea
Gustavo Petry <ogustavopetry@gmail.com>
2025-12-28 08:36:24 -0300

    Separando rotas em plugins

--- src/server.ts
+++ src/server.ts
@@ -2,26 +2,12 @@ import fastify from "fastify";
 import crypto from "node:crypto";
 import { knex } from "./database";
 import { env } from "./env";
+import { transactionRoutes } from "./routes/transactions";
 
 const app = fastify();
 
-app.get("/add", async () => {
-  const transaction = await knex("transactions").insert({
-    id: crypto.randomUUID(),
-    title: "Transação de Teste 2",
-    amount: 2000
-  })
-  .returning("*");
-
-  return transaction;
-});
-
-app.get("/search", async () => {
-  const transactions = await knex("transactions")
-    .where("amount", 2000)
-    .select("*");
-
-  return transactions;
+app.register(transactionRoutes, {
+  prefix: "transactions"
 });
 
 app
