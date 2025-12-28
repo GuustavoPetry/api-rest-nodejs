4e935f429107c42a40c2bd08b13ab8a453eb2371
GuustavoPetry <ogustavopetry@gmail.com>
2025-12-28 16:53:52 -0300

    Fundamento de testes automatizados

---
+++ src/tests/example.spec.ts
@@ -0,0 +1,15 @@
+import { expect, test } from "vitest";
+import request from "supertest";
+import { app } from "../app";
+
+test("O usuário consegue criar uma nova transação", async () => {
+   const response = await request(app.server)
+        .post("/transactions")
+        .send({
+            title: "New Transaction",
+            amount: 5000,
+            type: "credit"
+        });
+
+    expect(response.statusCode).toEqual(201);
+});
